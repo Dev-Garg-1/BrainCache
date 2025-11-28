8160ebf52175f9f96ec89e1504943b5a1d902702
Dev Garg <gargdev010300@gmail.com>
2025-11-28 18:13:34 +0530

    shadcn and neobrutalism init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,10 @@
 import './App.css'
-import { Button } from './components/ui/Button'
-import { PlusIcon } from './icons/PlusIcon'
-import { ShareIcon } from './icons/ShareIcon'
 
 function App() {
 
   return (
     <>
-      <Button text={"Add Content"} startIcon={<PlusIcon size={"md"}/>}/>
-
-      <Button text={"Share Brain"} variant={"secondary"} startIcon={<ShareIcon size={"md"}/>}/>    
+      
     </>
   )
 }
